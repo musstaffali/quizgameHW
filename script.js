eb1a63c56e1e2c2f0f669f8275d1ac868342e060
musstaffali <mamustafa@gtcc.edu>
2020-09-09 19:07:15 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var count = 160;
+var interval = setInterval(function(){
+  document.getElementById('count').innerHTML=count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Done';
+    // or...
+    alert("You're out of time!");
+  }
+}, 1000);
+
+
+
